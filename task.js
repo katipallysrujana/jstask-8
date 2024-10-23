3994bfa29f8657f3dbbe30ab42e21ac627a2c9e0
unknown <katipallysrujana@gmail.com>
2024-10-23 23:34:14 +0530

    control statements

---
+++ task.js
@@ -0,0 +1,113 @@
+//for loop
+//example-1
+//post increment
+for (var a = 1; a < 3; a++) {
+    console.log(a)
+}
+
+//example-2
+for (var a = 1; a <= 7; a++) {
+    console.log(a)
+}
+
+
+//post decrement
+//example-3
+for (a = 11; a > 6; a--) {
+    console.log(a)
+}
+
+
+//example-4
+for (var a = 100; a >= 90; a--) {
+    console.log(a)
+}
+
+
+//example-5
+for (var i = 0; i <= 5; i++) {
+}
+console.log(i)
+
+
+
+//while loop
+//post increment
+//example-1
+var a = 1;
+while (a <= 7) {
+    console.log(a)
+    a++
+}
+
+//example-2
+var a = 1;
+while (a < 3) {
+    console.log(a)
+    a++
+}
+
+//example-3
+var a = 1;
+while (a < 7) {
+    console.log(a)
+    a++
+}
+
+//post decrement
+//example-4
+var a = 10;
+while (a >= 6) {
+    console.log(a)
+    a--
+}
+
+
+//example-5
+var i = 1
+while (i < 4) {
+    i++
+}
+console.log(i)
+
+
+
+
+//do-while loop
+//post increment
+//example-1
+var a = 1;
+do {
+    a++
+    console.log(a)
+}
+while (a <= 5)
+
+
+//example-2
+
+var a = 1;
+do {
+    console.log(a)
+    a++
+}
+while (a >= 7)
+
+
+//post decrement
+//example-3
+var a = 10;
+do {
+    console.log(a)
+    a--
+}
+while (a >= 7)
+
+
+//example-4
+var a = 10;
+do {
+    a--
+    console.log(a)
+}
+while (a >= 7)
